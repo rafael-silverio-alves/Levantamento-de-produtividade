e9b5489e85986bc50296013439803a330cab64bf
rafael-silverio-alves <rafael.silverio@igarashi.com.br>
2026-04-30 11:31:49 -0300

    Deploy versão web do app

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2405306753"
+    serviceWorkerVersion: "4273664648"
   }
 });
